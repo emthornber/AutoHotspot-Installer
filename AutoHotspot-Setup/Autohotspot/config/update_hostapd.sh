713776eb85b8c45e84b4136d0c98a1cab957567b
emthornber <enchanted.systems@btinternet.com>
2022-05-13 11:25:28 +0100

    First draft of .epm file completed

---
+++ AutoHotspot-Setup/Autohotspot/config/update_hostapd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Create /etc/hostapd.conf from /usr/local/etc/autohs/hostapd.conf.in
+# and /usr/local/etc/autohs/autohotspot.conf
+#
+#   12 May, 2022 - E M Thornber
+#
+SCRDIR=`dirname $0`
+
+. $SCRDIR/autohotspot.conf
+
+wpa_country=$(`/usr/bin/awk -F'=' '/country/{ print $2 }' /etc/wpa_supplicant/wpa_supplicant.conf`)
+if [ -z $wpa_country ] ; then wpa_country="GB" ; fi
+
+# shellcheck disable=SC2154
+/usr/bin/awk -v CHAN="$ap_channel" \
+    -v SSID="$ap_ssid" \
+    -v PASS="$ap_password" \
+    -v CODE="$wpa_country" \
+    -f $SCRDIR/update_hostapd.awk \
+    $SCRDIR/hostapd.conf.in
+
